b2ab05da6f7032f953dabde23d3bdad7579c9858
Indu Thiyagrajan <80294512+Indumathi-Thiyagarajan@users.noreply.github.com>
2024-10-18 16:57:45 -0700

    Update objective2.py

--- sequence/objective2.py
+++ sequence/objective2.py
@@ -6,8 +6,6 @@ class Polygon_sequence:
         self._n = n
         self._R = R
         self.polygons = [Polygon(n,R) for n in range(3,n+1)]
-        # print(self.polygons)
-        #[Polygon(n=3, R=10), Polygon(n=4, R=10), Polygon(n=5, R=10), Polygon(n=6, R=10), Polygon(n=7, R=10), Polygon(n=8, R=10), Polygon(n=9, R=10), Polygon(n=10, R=10), Polygon(n=11, R=10), Polygon(n=12, R=10), Polygon(n=13, R=10), Polygon(n=14, R=10), Polygon(n=15, R=10), Polygon(n=16, R=10), Polygon(n=17, R=10), Polygon(n=18, R=10), Polygon(n=19, R=10), Polygon(n=20, R=10), Polygon(n=21, R=10), Polygon(n=22, R=10), Polygon(n=23, R=10), Polygon(n=24, R=10), Polygon(n=25, R=10)]
         
     def max_efficiency_polygon(self):
         efficiency_list = ()
