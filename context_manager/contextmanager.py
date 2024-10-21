8afa201a9ad293e6751fc8b46a2b7c672e5f9bfb
Indu Thiyagrajan <80294512+Indumathi-Thiyagarajan@users.noreply.github.com>
2024-10-21 00:11:44 -0700

    Update contextmanager.py

--- context_manager/contextmanager.py
+++ context_manager/contextmanager.py
@@ -2,46 +2,65 @@ import csv
 from collections import namedtuple
 
 # contextmanager in class setup
+import csv
+from collections import namedtuple
+import pandas as pd
 
-class DataIterator:
-    def __init__(self, fname): 
+class CSV_Reader_contextmanager:
+    def __init__(self, fname):
         self._fname = fname
-        self._f = None
+        self._file = None
         self._reader = None
-        self._Row = None
+        self._header = None
+
+    def header_cleaner(self, header):
+        return [item.replace(" ", "_") for item in header]
     
-    @staticmethod
-    def sanitize_header(header):
-        return header.replace(" ", "_").replace("-", "_")
-                
-    def __enter__(self):
-        self._f = open(self._fname, 'r')
-        self._reader = csv.reader(self._f)
-        headers = next(self._reader)  # Extract the header row
-        # print("headers", headers)
-        sanitized_headers = [DataIterator.sanitize_header(header) for header in headers]
-        # Create the named tuple class
-        self._Row = namedtuple('Row', sanitized_headers)
-        # print("Row", self._Row)
+    def named_tuple(self):
+        Car_Details = namedtuple('Car_Details', self._header)
+        return Car_Details
+    
+    def column_datatype_cleaning(self, row):
+        for column, value in zip(self._header, row):
+            if column in ['Summons_Number', 'Violation_Code']:
+                yield pd.to_numeric(value, errors='coerce')
+            elif column == 'Issue_Date':
+                yield pd.to_datetime(value, errors='coerce')
+            else:
+                yield value
+    
+    def enter(self):
+        self._file = open(self._fname, 'r', encoding='ISO-8859-1')
+        self._reader = csv.reader(self._file)
+        self._header = next(self._reader)  # Extract the header row
+        self._header = self.header_cleaner(self._header)  # Clean the header row
+        self.Car_details_named_tuple = self.named_tuple()  # Create named tuple
         return self
-    
-    def __exit__(self, exc_type, exc_value, exc_tb):
-        if not self._f.closed:
-            self._f.close()
-        return False
     
     def __iter__(self):
         return self
     
-    def __next__(self):
-        row = next(self._reader)
-        return self._Row(*row)
-    
-with DataIterator('nyc_parking_tickets_extract.csv') as data: 
-    for row in data:
-        print(row)
-        break
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._reader is not None:  # Checks if csv reader has been read or not
+            self._file.close()  # Close the file when done
 
+    def __next__(self):
+        if self._reader is None: # checks if csv reader has been read or not
+            self.enter() #is a CSV reader object that iterates over the rows of the file.
+        try:
+            current_row = next(self._reader)
+            cleaned_row = tuple(self.column_datatype_cleaning(current_row))
+            return self.Car_details_named_tuple(*cleaned_row)  # Return a named tuple
+        except StopIteration:
+            self._file.close()  # Close the file when done
+            raise StopIteration
+
+# Example usage
+file_path = r'/Users/indu/EPAi/generator_Assignment/nyc_parking_tickets_extract-1.csv'
+csv_data = CSV_Reader_contextmanager(file_path)
+
+for car_details in csv_data:
+    print(car_details)
 
 
 
